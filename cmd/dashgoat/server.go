f5f7cbd69bdac813ba387e29ea2e48e2c74f354f
Bjorn Jorgensen <github@bjornz.dk>
2020-08-20 11:17:51 +0200

    Add IPPORT as ENV in Dockerfile

--- cmd/dashgoat/server.go
+++ cmd/dashgoat/server.go
@@ -150,7 +150,7 @@ func deleteService(c echo.Context) error {
 func health(c echo.Context) error {
 	appHealthResult = &AppHealth{}
 
-	appHealthResult.APIVersion = "1.1.3"
+	appHealthResult.APIVersion = "1.1.4"
 
 	return c.JSON(http.StatusOK, appHealthResult)
 }

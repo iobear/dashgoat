0fd36667051920f8639c331e902f14525f48580f
Bjorn Jorgensen <20801364+iobear@users.noreply.github.com>
2023-12-18 18:27:02 +0100

    Change start script for dashGoat test

---
+++ tests/start-single.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+BASE_URL="http://localhost:2000"
+
+if [ -z "$GITHUB_WORKSPACE" ]; then
+    ./dashgoat $1 &
+else
+    $GITHUB_WORKSPACE/dashgoatbuild/dashgoat $1 &
+fi
+
+PID=$!  # Get the PID
+echo $PID > PID-SINGLE-$PID
+sleep 1
+
+#Check if PID exists.
+if ! kill -0 $PID 2>/dev/null
+then
+    echo "Error: dashgoat failed to start for instance $INSTANCE"
+    exit 1
+fi
+
+if [ "$2" = "no-health-test" ]
+then
+    echo "No /health test"
+    exit 0
+fi
+
+for i in {1..10}; do
+    sleep 1
+
+    STATUS=$(curl -s "$BASE_URL/health")
+
+    if [ "$(echo "$STATUS" | jq -r '.Ready')" = "true" ]; then
+        echo "dashGoat ready - OK"
+        exit 0
+    else
+        echo "dashGoat not ready $i"
+    fi
+done
+
+echo "dashGoat not ready, giving up"
+exit 1
---
+++ tests/stop-instances.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for pid_file in PID*; do
+    if [ -f "$pid_file" ]; then
+        kill $(cat "$pid_file")
+        rm $pid_file
+    fi
+done

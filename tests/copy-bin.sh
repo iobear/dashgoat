ae8c0c115d275a4632808e2d2926fe9c71c4017a
Bjorn Jorgensen <20801364+iobear@users.noreply.github.com>
2024-03-05 16:52:19 +0100

    Change copy to link

--- tests/copy-bin.sh
+++ tests/copy-bin.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
 echo
-echo "-- copy bin --"
-echo
+echo "-- link bin --"
 
-cp build/dashgoat .
+SOURCE_DIR="build/"
+
+# Determine the operating system
+OS=$(uname -s)
+
+# Determine the architecture
+ARCH=$(uname -m)
+
+
+echo "Found: $OS $ARCH"
+DGBIN=""
+
+if [[ "$OS" == "Darwin" ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        DGBIN="dashgoat-mac"
+    else
+        DGBIN="dashgoat-mac-arm"
+    fi
+fi
+
+if [[ "$OS" == "Linux" ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        DGBIN="dashgoat"
+    fi
+fi
+
+if [[ "$DGBIN" == "" ]]; then
+    echo "Unsupported system: $OS $ARCH"
+fi
+
+#Debug
+pwd
+ls
+
+rm -f dashgoat
+ln -s $SOURCE_DIR$DGBIN dashgoat

873b0df4d7f7b4b11d59470dc33c469a833c5759
Bjorn Jorgensen <bnj@danskecommodities.com>
2024-03-18 16:39:07 +0100

    Update pagerduty test

--- tests/pagerduty-test.sh
+++ tests/pagerduty-test.sh
@@ -10,7 +10,7 @@ UPDATE_KEY="changeme"
 # Define services with tags
 services_with_tags=(
     '"service": "nginx", "tags": ["web", "production"]'
-    '"service": "database", "tags": ["db", "production"]'
+    '"service": "database", "tags": ["db", "test"]'
     '"service": "cache", "tags": ["cache", "development"]'
 )
 
@@ -22,3 +22,16 @@ for service_with_tags in "${services_with_tags[@]}"; do
          --data "{\"host\": \"host-1\", $service_with_tags, \"status\": \"error\", \"message\": \"Slow service\",\"updatekey\": \"$UPDATE_KEY\"}"
 
 done
+
+
+echo "-- 2x Host-1 should be seen in PagerDuty"
+
+for service_with_tags in "${services_with_tags[@]}"; do
+
+    curl -X POST "$BASE_URL/update" \
+         -H "Content-Type: $CONTENT_TYPE" \
+         --data "{\"host\": \"host-2\", $service_with_tags, \"status\": \"warning\", \"message\": \"Not so slow service\",\"updatekey\": \"$UPDATE_KEY\"}"
+
+done
+
+echo "-- Host-2 should NOT be seen in PagerDuty"
